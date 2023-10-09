3f357c91839315d24c64004a8fd79983318dc439
SeongGyu <sgbae777@gmail.com>
2023-10-09 22:05:50 +0900

    주말

---
+++ Selfstudy/BOJ_25192_인사성밝은곰곰이.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+
+chat = [input().rstrip() for _ in range(N)]
+
+cnt = 0
+start = False
+first = set()
+for i in range(N):
+    if chat[i] == 'ENTER':
+        first = set()
+    else:
+        if chat[i] not in first:
+            cnt +=1
+            first.add(chat[i])
+
+
+print(cnt)
