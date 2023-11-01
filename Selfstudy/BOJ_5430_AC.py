4a910fb06abdfa321866eea6870ee91e8f58b1f3
SeongGyu <sgbae777@gmail.com>
2023-11-01 21:34:38 +0900

    1101

--- Selfstudy/BOJ_5430_AC.py
+++ Selfstudy/BOJ_5430_AC.py
@@ -3,13 +3,14 @@ from sys import stdin
 
 
 def solve():
+    # 함수 'R'이 나오면 뒤집어야 하지만
+    # 뒤집지 않고 표시만 해둠 -> 연산을 줄여 시간복잡도를 낮춤
+    # 'D'가 나오면 뒤집혀있으면 맨 뒷쪽 숫자를 지우고
+    # 정방향이라면 맨 앞쪽 숫자를 지우면된다
     is_reverse = False
-    for f in p:
+    for f in func:
         if f == 'R':
-            if is_reverse:
-                is_reverse = False
-            else:
-                is_reverse = True
+            is_reverse = not is_reverse
         else:
             if num:
                 if is_reverse:
@@ -29,26 +30,18 @@ for tc in range(T):
     func = stdin.readline().rstrip()
     n_len = int(stdin.readline().rstrip())
     x = stdin.readline().rstrip()
-    num = deque()
-    if n_len > 0:
-        num = deque(map(int, x[1:-1].split(',')))
-    p = ''
-    count = 0
-    for f in func:
-        if f == 'R':
-            count += 1
-        else:
-            if count % 2 == 0:
-                p += 'D'
-            else:
-                p += 'RD'
-            count = 0
-    if count % 2 != 0:
-        p += 'R'
+
+    if n_len:
+        # 슬라이싱과 split을 이용해서 함수 사용을 쉽게 만들어줌
+        x = x[1:-1].split(',')
+    else:
+        x = []
+
+    num = deque(x)
 
     if solve():
         if num:
-            print(f'[{",".join(num)}]')
+            print(f"[{','.join(list(num))}]")
         else:
             print('[]')
     else:
