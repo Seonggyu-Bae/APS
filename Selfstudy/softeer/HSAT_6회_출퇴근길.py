ccff97ecca1cf4774b2fe172f5efde1487e7251b
SeongGyu <sgbae777@gmail.com>
2023-11-07 20:51:50 +0900

    1107

--- Selfstudy/softeer/HSAT_6회_출퇴근길.py
+++ Selfstudy/softeer/HSAT_6회_출퇴근길.py
@@ -4,8 +4,6 @@ from collections import deque
 
 
 def solve(start, end):
-    visited = [0] * (N + 1)
-
     q = deque()
     q.append(start)
     while q:
@@ -16,8 +14,10 @@ def solve(start, end):
             if not visited[go]:
                 q.append(go)
                 visited[go] = 1
-            if not lineinfo[go] and go != end:
-                visited[go] = 0
+            #
+            # if not lineinfo[go] and go != end:
+            #     print(not lineinfo[go])
+            #     visited[go] = 0
 
     my_set = set()
     for idx, v in enumerate(visited):
@@ -26,17 +26,27 @@ def solve(start, end):
     return my_set
 
 
+# 방문한 정점에서 목적지 까지 갈 수 있는 방법이 있어야함
+
+
 N, M = map(int, input().split())  # N: 정점, M: 간선의 개수
 
-# lineinfo = [[0] * (N + 1) for _ in range(N + 1)]
-lineinfo = [[] for _ in range(N+1)]
+lineinfo = [[] for _ in range(N + 1)]
+
 for _ in range(M):
     x, y = map(int, input().split())
     lineinfo[x].append(y)
 
 S, T = map(int, input().split())
 
+visited = [0] * (N + 1)
 
 going = solve(S, T)
+for x in going:
+    print(solve(x,T))
+print(going)
 back = solve(T, S)
+for x in back:
+    print(solve(x,S))
+print(back)
 print(len(going.intersection(back)))
